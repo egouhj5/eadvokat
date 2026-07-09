1030ebfe45c63fcddf85ebcbf0398335f74a26d1
egouhj5 <Kn09032010@gmail.com>
2026-07-09 13:49:34 +0200

    header-update

--- js/navigation-manu.js
+++ js/navigation-manu.js
@@ -1,13 +1,14 @@
 const button = document.querySelector(".menu-button")
 const navigationMenu = document.querySelector(".header-menu-navigation")
 const logo = document.querySelector(".logo")
+const menuIcon = document.querySelector(".menu-icon");
 let buttonClicked = false;
 button.addEventListener("click", ()=> {
     if (buttonClicked == false) {
         navigationMenu.style.display = "flex"
         logo.style.padding = "0 0 20px 0"
         logo.style.margin = "0 0 20px 0"
-        logo.style.borderBottom = "3px solid #1a4735"
+        logo.style.borderBottom = "3px solid #1b6044"
         buttonClicked = !buttonClicked
     } else if (buttonClicked == true) {
         logo.style.padding = "0"
@@ -15,5 +16,6 @@ button.addEventListener("click", ()=> {
         logo.style.border = "none"
         navigationMenu.style.display = "none"
         buttonClicked = !buttonClicked
-    } 
+    }
+    menuIcon.classList.toggle("is-active");
 });
